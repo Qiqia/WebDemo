682b76bc3999aab38f794d65a627e930e0f04b79
zsl <zhusiliang1991@163.com>
2018-09-04 14:09:11 +0800

    demo

---
+++ BootDemo/src/main/java/com/example/BootDemo/UserController.java
@@ -0,0 +1,30 @@
+package com.example.BootDemo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by zsl on 2018/9/4.
+ */
+//@RestController注解能够使项目支持Rest
+@RestController
+@SpringBootApplication
+@RequestMapping(value = "/springboot")
+public class UserController {
+    @RequestMapping(value = "/getUserByGet",method = RequestMethod.GET)
+    String getUserByGet(@RequestParam(value = "userName")String userName){
+        System.out.println("Hello "+userName);
+        return "Hello "+userName;
+    }
+
+    @RequestMapping(value = "/getUserByPost",method = RequestMethod.POST)
+    String getUserByPost(@RequestParam(value = "userName")String userName){
+        System.out.println("Hello "+userName);
+        return "Hello "+userName;
+    }
+
+    @RequestMapping(value = "getUserByJson",method = RequestMethod.POST)
+    String getUserByJson(@RequestBody String data){
+        return "Json is " +data;
+    }
+}
