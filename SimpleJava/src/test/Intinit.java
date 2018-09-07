64de9b29d6f84d89960acfc155bd42e9e8d6af93
zsl <zhusiliang1991@163.com>
2018-09-07 15:56:19 +0800

    lll

--- SimpleJava/src/test/Intinit.java
+++ SimpleJava/src/test/Intinit.java
@@ -3,8 +3,15 @@
  */
 public class Intinit {
     public static void main(String[] args) {
-        int i=0;
-        System.out.println(i);
+//        int i=0;
+//        System.out.println(i);
+        int i = 1;
+        double j = 1.0000000000000001;
+        System.out.println(i==j);
+        int l = 1;
+        double m = 1.000000000000001;
+        System.out.println(l==m);
+
     }
 }
 
