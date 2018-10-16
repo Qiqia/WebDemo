9a4914a6488b056273e51df4689fada83b3123b9
zsl <zhusiliang1991@163.com>
2018-10-16 10:48:10 +0800

    easyexcel写文件

--- DemoOne/src/main/java/example/zsltest/EasyExcelDemo.java
+++ DemoOne/src/main/java/example/zsltest/EasyExcelDemo.java
@@ -3,13 +3,17 @@ package example.zsltest;
 
 import com.alibaba.excel.ExcelReader;
 import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.annotation.ExcelProperty;
 import com.alibaba.excel.metadata.BaseRowModel;
 import com.alibaba.excel.metadata.Sheet;
 import com.alibaba.excel.read.context.AnalysisContext;
 import com.alibaba.excel.read.event.AnalysisEventListener;
 import com.alibaba.excel.support.ExcelTypeEnum;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -19,6 +23,7 @@ public class EasyExcelDemo {
     public static void main(String[] args) throws Exception{
         EasyExcelDemo demo = new EasyExcelDemo();
         demo.noModelMultipleSheet();
+        demo.writeExcelTest();
     }
 
     public void noModelMultipleSheet() throws Exception{
@@ -52,30 +57,75 @@ public class EasyExcelDemo {
     }
 
 
-//    public void writeExcelTest()throws FileNotFoundException{
-//        OutputStream outputStream = new FileOutputStream(new File("D:/sheetdemo1.xlsx"));
-//        try {
-//            ExcelWriter writer = new ExcelWriter(outputStream, ExcelTypeEnum.XLSX);
-//            //写第一个sheet, sheet1  数据全是List<String> 无模型映射关系
-//            Sheet sheet1 = new Sheet(1, 0,ExcelPropertyIndexModel.class);
-//            writer.write(getData(), sheet1);
-//            writer.finish();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        } finally {
-//            try {
-//                outputStream.close();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
+    public void writeExcelTest() throws FileNotFoundException{
+        OutputStream outputStream = new FileOutputStream(new File("D:\\sheetdemo1.xlsx"));
+        try {
+            ExcelWriter writer = new ExcelWriter(outputStream, ExcelTypeEnum.XLSX);
+            //写第一个sheet, sheet1  数据全是List<String> 无模型映射关系
+            Sheet sheet1 = new Sheet(1, 0,ExcelPropertyIndexModel.class);
+            writer.write(getData(), sheet1);
+            writer.finish();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private List<? extends BaseRowModel> getData() {
+//        ExcelPropertyIndexModel excel = new ExcelPropertyIndexModel("xxxx","27");
+//        excel.setName("zsl");
+//        excel.setAge("27");
+//        List<ExcelPropertyIndexModel> excelList = new ArrayList<>();
+//        excelList.add(excel);
+//        return  excelList;
+
+        return new ArrayList<ExcelPropertyIndexModel>(){{
+            add(new ExcelPropertyIndexModel("zhangsan","23") );
+            add(new ExcelPropertyIndexModel("lisi","24") );
+            add(new ExcelPropertyIndexModel("wangwu","25") );
+            add(new ExcelPropertyIndexModel("zhaoliu","26") );
+        }};
+    }
+
+
+    //这个类必须要public 否则 会报NoMethodException 也就是获取不到getter和setter方法
+    public class ExcelPropertyIndexModel extends BaseRowModel {
+
+        @Getter
+        @Setter
+        @ExcelProperty(value = "姓名", index = 0)
+        private String name;
+
+        @Getter
+        @Setter
+        @ExcelProperty(value = "年龄", index = 1)
+        private String age;
+
+        public ExcelPropertyIndexModel(String name,String age){
+            this.name = name;
+            this.age = age;
+        }
+
+//        public String getName() {
+//            return name;
 //        }
-//    }
 //
-//    private List<? extends BaseRowModel> getData() {
+//        public void setName(String name) {
+//            this.name = name;
+//        }
 //
+//        public String getAge() {
+//            return age;
+//        }
 //
-//    }
-//
-//    private class ExcelPropertyIndexModel extends BaseRowModel {
-//    }
+//        public void setAge(String age) {
+//            this.age = age;
+//        }
+
+    }
 }
