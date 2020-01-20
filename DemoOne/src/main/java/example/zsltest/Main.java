0f0a8ecef7d3234d8ebfbbae4147e40210da80ee
zsl <zhusiliang1991@163.com>
2020-01-20 17:07:49 +0800

    instanceof测试

---
+++ DemoOne/src/main/java/example/zsltest/Main.java
@@ -0,0 +1,9 @@
+package example.zsltest;
+
+public class Main {
+    public static void main(String[] args) {
+        Son son = new Son();
+        System.out.println(son instanceof Father);
+
+    }
+}
